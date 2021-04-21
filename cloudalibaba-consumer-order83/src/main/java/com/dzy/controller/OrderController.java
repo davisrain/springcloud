be87604b8d45e13b89e2ee7145cf36b4c2f330fc
dzy <zhengyu_dai@foxmail.com>
2021-04-21 19:06:59 +0800

    commit springcloud from company computer

---
+++ cloudalibaba-consumer-order83/src/main/java/com/dzy/controller/OrderController.java
@@ -0,0 +1,22 @@
+package com.dzy.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class OrderController {
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Value("${payment.url}")
+    String paymentUrl;
+
+    @GetMapping("/consumer/payment/nacos/{id}")
+    public String getServerPort(@PathVariable("id") Integer id) {
+        return restTemplate.getForObject(paymentUrl + "/payment/nacos/" + id, String.class);
+    }
+}
